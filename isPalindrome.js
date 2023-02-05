41d0844cb32df83453828a9d56ce895739e8fb8e
Lily Springer <lilyspringer@hotmail.com>
2023-02-04 21:13:10 -0700

    first commit

---
+++ isPalindrome.js
@@ -0,0 +1,13 @@
+// Write your code below
+
+function isPalindrome(str) {
+    let x = Math.floor(str.length / 2);
+    for (let i = 0; i < x; i++){
+        if (str[i] !== str[str.length - i - 1]){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPalindrome(`racecar`))
